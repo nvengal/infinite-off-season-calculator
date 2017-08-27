8bdb3dfd4cf672d068c085727f3e66795b839138
Nikhil Vengal <nikhilvengal@gmail.com>
2017-08-26 22:26:38 -0400

    removed db credentials from config file...

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
+  database: 'mongodb://'+process.env.DB_USER+':'+process.env.DB_PASSWORD+'@ds157723.mlab.com:57723/infinite-off-season-db',
   database: 'mongodb://nvengal:nvengal@ds157723.mlab.com:57723/infinite-off-season-db',
   secret: 'secret'
 }
